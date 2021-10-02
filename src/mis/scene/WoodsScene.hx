877509c1cb09c53f7289cf149e43432194bbc8e9
Matt Swieboda <mswieboda@users.noreply.github.com>
2021-10-01 21:42:59 -0500

    adds tree, player placeholders

--- src/mis/scene/WoodsScene.hx
+++ src/mis/scene/WoodsScene.hx
@@ -14,19 +14,37 @@ import hxd.res.DefaultFont;
 class WoodsScene extends GameScene {
   public static inline var SPEED = 100;
 
-  var tree : Obj;
+  var player : Obj;
+  var fallingTree : Obj;
 
   public function new(stage : Stage) {
     super(stage, "Wiley Woods");
 
-    var tile = Tile.fromColor(0x00FF00, 10, 50);
+    var tile = Tile.fromColor(0x00cc00, 16, 64);
+
+    // background trees
+    var treesData = [
+      {x: 100, y: 200},
+      {x: 500, y: 300},
+      {x: 600, y: 600},
+    ];
+
+    for (treeData in treesData) {
+      var treeSprite = new Bitmap(tile, s2d);
+      var tree = new Obj(treeSprite, null, null, s2d);
+      tree.x = treeData.x;
+      tree.y = treeData.y;
+    }
+
     var treeSprite = new Bitmap(tile, s2d);
+    fallingTree = new Obj(treeSprite, null, null, s2d);
+    fallingTree.x = 500;
+    fallingTree.y = 300;
 
-    tree = new Obj(treeSprite, null, null, null);
-    tree.x = 100;
-    tree.y = 300;
-
-    s2d.add(tree, 0);
+    var playerSprite = new Bitmap(Tile.fromColor(0xcc0000, 16, 48));
+    player = new Obj(playerSprite, null, null, s2d);
+    player.x = 500;
+    player.y = 500;
   }
 
   public override function update(dt: Float) {
@@ -37,9 +55,9 @@ class WoodsScene extends GameScene {
     }
 
     if (Input.game.isDown("moveLeft")) {
-      tree.x -= dt * SPEED;
+      player.x -= dt * SPEED;
     } else if (Input.game.isDown("moveRight")) {
-      tree.x += dt * SPEED;
+      player.x += dt * SPEED;
     }
   }
 
