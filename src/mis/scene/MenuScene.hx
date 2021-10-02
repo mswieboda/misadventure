b550a5191d50b656ab8d55b0bc541f9fbc05f018
Matt Swieboda <mswieboda@users.noreply.github.com>
2021-10-01 19:19:38 -0500

    adds header to main menu

--- src/mis/scene/MenuScene.hx
+++ src/mis/scene/MenuScene.hx
@@ -5,14 +5,25 @@ import hxt.scene.Stage;
 import hxt.scene.Scene;
 import hxt.ui.Menu;
 
+import h2d.HtmlText;
 import h3d.Vector;
 import hxd.System;
+import hxd.res.DefaultFont;
 
 class MenuScene extends Scene {
   var menu : Menu;
 
   public function new(stage : Stage) {
     super(stage);
+
+    // NOTE: DefaultFont is 12, so keep factor of 12 for best results
+    var headerFont = DefaultFont.get().clone();
+    headerFont.resizeTo(36);
+
+    var header = new HtmlText(headerFont, s2d);
+    header.text = '<font color="#ff6600">mis</font><font opacity="0.333">adventure</font>';
+    header.x = s2d.width / 2 - header.textWidth / 2;
+    header.y = s2d.height / 5 - header.textHeight / 2;
 
     var menuItemData = [{
       text: "start",
@@ -23,7 +34,11 @@ class MenuScene extends Scene {
       action: () -> System.exit()
     }];
 
-    menu = new Menu(s2d, menuItemData, s2d.width / 2, 0x00FF00);
+    // NOTE: DefaultFont is 12, so keep factor of 12 for best results
+    var menuFont = DefaultFont.get().clone();
+    menuFont.resizeTo(24);
+
+    menu = new Menu(s2d, menuItemData, s2d.width / 2, 0xFF6600, null, menuFont);
     menu.y = s2d.height / 3;
   }
 
